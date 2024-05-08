95aa17b1b669dd1214321e6058f0eb2abc7c1828
DuongNHH <duongss.company@gmail.com>
2024-05-08 22:16:03 +0700

    - Compare ui

--- app/src/main/java/com/dss/xeapplication/ui/compare/CompareFragment.kt
+++ app/src/main/java/com/dss/xeapplication/ui/compare/CompareFragment.kt
@@ -5,8 +5,6 @@ import com.dss.xeapplication.base.BaseFragment
 import com.dss.xeapplication.base.ViewPagerAdapter
 import com.dss.xeapplication.base.extension.removeSelf
 import com.dss.xeapplication.databinding.FragmentCompareBinding
-import com.dss.xeapplication.ui.compare.page.PageCarCompare
-import com.dss.xeapplication.ui.compare.page.PageResult
 import com.dss.xeapplication.ui.main.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -25,30 +23,13 @@ class CompareFragment : BaseFragment<FragmentCompareBinding>() {
 
     }
 
-    private lateinit var viewPagerAdapter: ViewPagerAdapter
-
-    private lateinit var arrayTabFragment: ArrayList<BaseFragment<*>>
-
     private val viewModel by activityViewModels<MainViewModel>()
 
     override fun initConfig() {
         super.initConfig()
 
-
-        initPage()
     }
 
-    private fun initPage() {
-        arrayTabFragment = arrayListOf(
-            PageCarCompare.newInstance(), PageResult.newInstance(), PageCarCompare.newInstance()
-        )
-        viewPagerAdapter = ViewPagerAdapter(
-            childFragmentManager, lifecycle, arrayTabFragment
-        )
-        binding.vpData.offscreenPageLimit = arrayTabFragment.size
-        binding.vpData.adapter = viewPagerAdapter
-        binding.vpData.currentItem = PAGE_RESULT
-    }
 
     override fun initObserver() {
         super.initObserver()
@@ -58,15 +39,8 @@ class CompareFragment : BaseFragment<FragmentCompareBinding>() {
     override fun initListener() {
         super.initListener()
 
-        binding.btnBack.setOnClickListener {
 
-        }
-
-        binding.btnForward.setOnClickListener {
-
-        }
-
-        backListener(binding.btnClose) {
+        backListener(binding.ivClose) {
             removeSelf()
         }
     }
