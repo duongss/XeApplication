d62785c5ce99118a98e5be2103f91af63ec7cc9f
DuongNHH <duongss.company@gmail.com>
2024-07-11 22:59:00 +0700

    -  mew ui

--- app/src/main/java/com/dss/xeapplication/ui/main/page/selection/SelectionFragment.kt
+++ app/src/main/java/com/dss/xeapplication/ui/main/page/selection/SelectionFragment.kt
@@ -5,7 +5,10 @@ import com.dss.xeapplication.base.BaseFragment
 import com.dss.xeapplication.base.extension.gone
 import com.dss.xeapplication.base.extension.onAvoidDoubleClick
 import com.dss.xeapplication.base.extension.removeSelf
+import com.dss.xeapplication.data.FirebaseStorage
+import com.dss.xeapplication.databinding.ChipfilterBinding
 import com.dss.xeapplication.databinding.FragmentSelectionBinding
+import com.google.android.material.chip.Chip
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -25,11 +28,52 @@ class SelectionFragment : BaseFragment<FragmentSelectionBinding>() {
         binding.toolbar.title.text = getString(R.string.selection)
         binding.toolbar.btnBack.gone()
 
+        initChipFuel()
+        initChipConvenient()
+        initChipSeat()
+    }
+
+    private fun initChipConvenient() {
+        listOf(R.string.low,R.string.medium,R.string.high).forEach {
+            val chip = createChip(it)
+            binding.chipGroupEngine.addView(chip)
+        }
+    }
+    private fun initChipSeat() {
+        FirebaseStorage.listCar.map { it.numOfSeats }.toSet().forEach {
+            val chip = createChip(it.toString())
+            binding.chipGroupSeat.addView(chip)
+        }
+    }
+
+    private fun initChipFuel() {
+        listOf(R.string.oil_or_gas,R.string.electric,R.string.hybrid).forEach {
+            val chip = createChip(it)
+            binding.chipGroupFuel.addView(chip)
+        }
+    }
+
+    private fun createChip(t:Int): Chip {
+       return createChip(getString(t))
+    }
+
+    private fun createChip(str:String) : Chip {
+        val chip = ChipfilterBinding.inflate(layoutInflater).root
+        chip.apply {
+            text = str
+            isCloseIconVisible = false
+            isCheckedIconVisible = true
+        }
+        return chip
     }
 
     override fun initListener() {
         backListener(binding.toolbar.btnBack) {
             removeSelf()
+        }
+
+        binding.btnSet.onAvoidDoubleClick {
+
         }
     }
 }
